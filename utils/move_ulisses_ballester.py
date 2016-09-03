40dd652fa33781cf87551fb9e9e8cd02ba921617
Ballester <pedballester@gmail.com>
2016-09-03 09:01:59 -0300

    committing move_ulisses_ballester

---
+++ utils/move_ulisses_ballester.py
@@ -0,0 +1,31 @@
+from glob import glob
+import shutil
+
+names = glob('/home/ulisses/SubImageNet')
+
+"""Open reference file"""
+sketch_classes = []
+sketch_names = []
+with open('one-hot_references.txt') as fid:
+    # dump
+    fid.readline()
+    one_hots = []
+    one_hots_values = []
+    for i in range(0, 57):
+        aux = fid.readline()
+        aux = aux.split(':')
+        sketch_classes.append(int(aux[1]))
+        sketch_names.append(aux[0])
+
+synsets = []
+synsets_names = []
+with open('synset_words.txt') as fid:
+    for line in fid:
+        line = line.split(' ')
+        synsets.append(line[0])
+        synsets_names.append(line[1:])
+
+for name in names:
+    name = name.split('_')
+    print name[0], sketch_names[synsets.index(name[0])]
+
